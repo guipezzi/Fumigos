033daa7643ffa9ab846400fd602e9fe29fdd7dde
Guilherme de Araujo Pezzi Nunes <guilherme.nunes@fumecacad.local>
2025-11-13 12:13:35 -0300

    0.7

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
+import accounts.views as views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup_view, name="signup"),
+    path('login/',views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
+#Funcao de Cadastro
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    #Para requisicoes POST
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            #logar o usuario apos cadastro
+            login(request, user)
+            return redirect('articles:list')
+    #Para requisicoes GET
+    else:
+        form = UserCreationForm()
+    return render(request,'accounts/signup.html', { 'form':form })
+
+#Funcao de Login
+def login_view(request):
+    #Para requisicoes POST
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #logar o usuario
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:list')
+    #Para requisicoes GET
+    else:
+        form = AuthenticationForm()
+    return render(request,'accounts/login.html', { 'form':form })
+
+#Funcao de Logout
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
     

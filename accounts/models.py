536aefc975a5a2f082dfd74c386975d502b8d182
Bernardo Giudicelli <giudicelli.be@gmail.com>
2025-11-17 11:28:20 -0300

    feat: first user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', default='default.png')
+
+    def __str__(self):
+        return self.display_name or self.user.username
+

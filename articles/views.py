0eaff9ad45482fa9bede1879fa1d278b244a737d
Bernardo Giudicelli <giudicelli.be@gmail.com>
2025-11-18 17:41:01 -0300

    feat: new article page using bootstrap

--- articles/views.py
+++ articles/views.py
@@ -5,11 +5,10 @@ from django.contrib.auth.decorators import login_required
 from . import forms
 
 def article_list(request):
-    articles = Article.objects.all().order_by('date') #Recupera todos os objetos de artigos ordenados por data
-    return render(request, 'articles/article_list.html', {'articles':articles} ) #Renderiza o template da lista de artigos e associa a variavel artigos
+    articles = Article.objects.all().order_by('date') 
+    return render(request, 'articles/article_list.html', {'articles':articles} )
 
-def article_detail(request, slug): #Captura a URL digitada pelo usuario
-    #return HttpResponse(slug) 
+def article_detail(request, slug): 
     article = Article.objects.get(slug=slug)
     return render(request,'articles/article_detail.html', {'article':article})
 
@@ -18,12 +17,10 @@ def article_create(request):
     if request.method == 'POST':
         form = forms.CreateArticle(request.POST, request.FILES)
         if form.is_valid():
-            #salvar artigo no banco de dados
             instance =form.save(commit=False)
             instance.author = request.user
             instance.save()
-            return redirect('articles:list')
+            return redirect("accounts:profile", username=request.user.username)
     else:
         form = forms.CreateArticle()
-    # Quando o usuário faz login, será redirecionado de volta para /articles/create/ através do parâmetro 'next'
     return render(request, 'articles/article_create.html', { 'form': form })
